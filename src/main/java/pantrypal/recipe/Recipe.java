3a4074d4ba79ff86180bfe669f7e62a95398bdb7
minhbgn <nhatminh003@e.ntu.edu.sg>
2025-03-15 18:35:36 +0800

    Add more functions
    Added editRecipe functions. Completed v1.0 for Recipe branch

--- src/main/java/pantrypal/recipe/Recipe.java
+++ src/main/java/pantrypal/recipe/Recipe.java
@@ -1,14 +1,16 @@
 package pantrypal.recipe;
 
-import pantrypal.inventory.Ingredient;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Recipe {
 
     private static final String LINE = "_".repeat(50);
 
     private String name;
-    private ArrayList<String> instructions;
+    private ArrayList<Instruction> instructions;
     private ArrayList<String> ingredients;
 
     public Recipe(){
@@ -23,26 +25,79 @@ public class Recipe {
         return name;
     }
 
-    public String getInstructions(){
-        String allInstructions = "";
-        for (String instruction : instructions) {
-            allInstructions = allInstructions + instruction + "\n";
+
+    public boolean addInstruction(Instruction instruction) {
+        if (instructions.contains(instruction)) {
+            System.out.println("Error: Instruction at step " + instruction.getStep() + " already exists.");
+            return false; // Duplicate item not added
         }
-        return allInstructions;
+        instructions.add(instruction);
+        instructions.sort(Comparator.comparingInt(Instruction::getStep));
+        return true; // Successfully added
     }
 
-    public String getIngredients(){
-        String allIngredients = "";
-        for (String ingredient : ingredients) {
-            allIngredients = allIngredients + ingredient + "\n";
+    public Instruction getInstruction(int index) {
+        List<Instruction> filtered = instructions.stream()
+                .filter(i -> i.getStep() == index).toList();
+
+        if (filtered.isEmpty()) {
+            return null;
         }
-        return allIngredients;
+
+        return filtered.get(0);
+    }
+
+    public void removeInstruction(int index) {
+        List<Instruction> filtered = instructions.stream()
+                .filter(i -> i.getStep() == index).toList();
+        if (filtered.isEmpty()) {
+            System.out.println("Error: Instruction at step " + index + " does not exist.");
+            return;
+        }
+        instructions.remove(index);
+    }
+
+    public String getAllInstructions(){
+        StringBuilder allInstructions = new StringBuilder();
+        for (Instruction instruction : instructions) {
+            allInstructions.append(instruction.toString()).append("\n");
+        }
+        return allInstructions.toString();
+    }
+
+    public void addIngredient(String ingredient) {
+        if (ingredients.contains(ingredient)) {
+            System.out.println("Error: Ingredient " + ingredient + " already exists.");
+        }
+        ingredients.add(ingredient);
+    }
+
+    public void editIngredient(String ingredient, String newIngredient) {
+        if (!ingredients.contains(ingredient)) {
+            System.out.println("Error: Ingredient " + ingredient + " does not exist.");
+        }
+        ingredients.set(ingredients.indexOf(ingredient), newIngredient);
+    }
+
+    public void removeIngredient(String ingredient) {
+        if (!ingredients.contains(ingredient)) {
+            System.out.println("Error: Ingredient " + ingredient + " does not exist.");
+        }
+        ingredients.remove(ingredient);
+    }
+
+    public String getAllIngredients(){
+        StringBuilder allIngredients = new StringBuilder();
+        for (String ingredient : ingredients) {
+            allIngredients.append(ingredient).append("\n");
+        }
+        return allIngredients.toString();
     }
 
     public String getContent(){
         String allContents = "";
         allContents = allContents + name + "\n" + LINE + "\n" + "Instructions:" + "\n"
-                + getInstructions() + "\n" + LINE + "\n" + "Ingredients:" + "\n";
+                + getAllInstructions() + "\n" + LINE + "\n" + "Ingredients:" + "\n" + getAllIngredients() + "\n";
         return allContents;
     }
 
--- src/main/java/pantrypal/recipe/RecipeManager.java
+++ src/main/java/pantrypal/recipe/RecipeManager.java
@@ -1,7 +1,6 @@
 package pantrypal.recipe;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RecipeManager {
@@ -11,6 +10,12 @@ public class RecipeManager {
     public RecipeManager() {}
 
     public void addRecipe(String recipeName) {
+
+        String[] parts = recipeName.split(" ", 2);
+        if (parts.length > 1){
+            System.out.println("Warning: Recipe name should not contain space. Use '_' instead.");
+            return;
+        }
 
         List<Recipe> filteredItems = recipes.stream()
                 .filter(item -> recipeName.equals(item.getName()))
@@ -23,7 +28,7 @@ public class RecipeManager {
 
         Recipe recipe = new Recipe(recipeName);
         recipes.add(recipe);
-        System.out.println("Recipe added sucessfully" + recipe.toString());
+        System.out.println("Recipe added sucessfully" + recipe);
     }
 
     public void editRecipe(String command) {
@@ -46,31 +51,89 @@ public class RecipeManager {
 
         if (parts[1].equals("instruction")){
             if (parts[2].equals("add")){
-                //Add function
+                addRecipeInstruction(recipe, parts[3], parts[4]);
             }
             else if (parts[2].equals("remove")){
-                //Add function
+                removeRecipeInstruction(recipe, parts[3]);
+            }
+            else if (parts[2].equals("edit")){
+                editRecipeInstruction(recipe, parts[3], parts[4]);
             }
             else {
                 //throw new Exception();
+                System.out.println("Wrong editRecipe format!");
+                System.out.println("Expected add/remove/edit after 'instruction'");
             }
 
         }
         else if (parts[1].equals("ingredient")) {
             if (parts[2].equals("add")){
                 //Add function
+                recipe.addIngredient(parts[3]);
             }
             else if (parts[2].equals("remove")){
                 //Add function
+                recipe.removeIngredient(parts[3]);
+            }
+            else if (parts[2].equals("edit")){
+                recipe.editIngredient(parts[3], parts[4]);
             }
             else {
+                System.out.println("Wrong ingredient format!");
+                System.out.println("Expected add/remove/edit after 'ingredient'");
                 //throw new Excpetion()
             }
         }
         else {
+            System.out.println("Wrong editRecipe format!");
+            System.out.println("Expected instruction/ingredient after 'editRecipe'");
             //throw new Exception();
         }
 
+    }
+
+    public void addRecipeInstruction(Recipe recipe, String stepString, String content) {
+        try {
+            int stepInt = Integer.parseInt(stepString);
+            Instruction instruction = new Instruction(stepInt, content);
+            recipe.addInstruction(instruction);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Warning: Invalid instruction");
+            System.out.println("The correct format is: editRecipe <recipe_name> instruction add <step> <content>\n" +
+                    "Example: editRecipe fried_egg instruction add 3 'add salt'");
+        }
+    }
+
+    public void editRecipeInstruction(Recipe recipe, String stepString, String content) {
+        try{
+            int stepInt = Integer.parseInt(stepString);
+            Instruction instruction = recipe.getInstruction(stepInt);
+            if (instruction == null) {
+                System.out.println("There is no instruction for step " + stepInt);
+                return;
+            }
+
+            String instructionContent = content;
+            instruction.setInstruction(instructionContent);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Warning: Invalid instruction");
+            System.out.println("The correct format is: editRecipe <recipe_name> instruction edit <step> <content>\n" +
+                    "Example: editRecipe fried_egg instruction edit 3 'add salt'");
+        }
+    }
+
+    public void removeRecipeInstruction(Recipe recipe, String stepString) {
+        try{
+            int stepInt = Integer.parseInt(stepString);
+            recipe.removeInstruction(stepInt);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Warning: Invalid instruction");
+            System.out.println("The correct format is: editRecipe <recipe_name> instruction remove <step>\n" +
+                    "Example: editRecipe fried_egg instruction remove 3");
+        }
     }
 
     public void listRecipe(){
