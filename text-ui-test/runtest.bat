baa29c799cfa4d13af6bd0c04e14b6bc1a43b98c
Zhan Hao <zhanhaoh@u.nus.edu>
2025-03-17 01:48:27 +0800

    bug fix: runtest.bat and runtest.sh

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -5,13 +5,15 @@ pushd %~dp0
 cd ..
 call gradlew clean shadowJar
 
-cd out\artifacts\tp_main_jar
-for /f "tokens=*" %%a in ('dir /b *.jar') do (
+cd build\libs
+for /f "tokens=*" %%a in (
+    'dir /b *.jar'
+) do (
     set jarloc=%%a
 )
 
-java -jar %jarloc% < ..\..\..\text-ui-test\input.txt > ..\..\..\text-ui-test\ACTUAL.TXT
+java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
 
-cd ..\..\..\text-ui-test
+cd ..\..\text-ui-test
 
 FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,29 +1,19 @@
-#!/usr/bin/env bash
-
-# Change to script directory
-cd "${0%/*}"
+@echo off
+setlocal enableextensions
+pushd %~dp0
 
 cd ..
-./gradlew clean shadowJar
+call gradlew clean shadowJar
 
-cd out/artifacts/tp_main_jar
+cd build\libs
+for /f "tokens=*" %%a in (
+    'dir /b *.jar'
+) do (
+    set jarloc=%%a
+)
 
-# Find the .jar file
-JARLOC=$(find . -name "*.jar" -print -quit)
+java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
 
-cd ../../text-ui-test
+cd ..\..\text-ui-test
 
-java -jar "$JARLOC" < input.txt > ACTUAL.TXT
-
-# Convert EXPECTED.TXT to UNIX format and compare with ACTUAL.TXT
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
-diff EXPECTED-UNIX.TXT ACTUAL.TXT
-
-if [ $? -eq 0 ]; then
-    echo "Test passed!"
-    exit 0
-else
-    echo "Test failed!"
-    exit 1
-fi
+FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
