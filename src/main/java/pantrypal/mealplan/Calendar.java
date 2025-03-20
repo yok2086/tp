4fbd4fc447d9c2fc332063d92aae6f1c565c2f84
srfl0-admin <e1121549@u.nus.edu>
2025-03-20 23:20:12 +0800

    Modify mealplan package structure

--- src/main/java/pantrypal/mealplan/Calendar.java
+++ src/main/java/pantrypal/mealplan/Calendar.java
@@ -1,16 +1,61 @@
 package pantrypal.mealplan;
 
-public class Calendar {
-    String month;
-    int startDate;
-    int endDate;
-    MealPlan mealPlan;
+import java.util.ArrayList;
 
-    public Calendar(String month, int startDate, int duration, MealPlan mealPlan) {
-        this.month = month;
-        this.startDate = startDate;
-        this.endDate = startDate+duration;
-        this.mealPlan = mealPlan;
+public class Calendar {
+    String userName;
+    int selectedPlan;
+    //ArrayList<MealPlan> plans;
+    ArrayList<MealPlan> planPresets;
+
+    public Calendar(PlanPresets planPresets, String userName) {
+        this.planPresets = planPresets.plans;
+        this.userName = userName;
+    }
+
+    /*
+     *
+     * @param planIndex refers to the plan slot in a preset
+     */
+
+    /*
+    public void AddPlanToCalendar(int planIndex){
+        plans.add(planPresets.get(planIndex));
+    }
+    */
+
+    /*
+     *
+     * @param planIndex refers to the plan in this calendar
+     */
+
+    /*
+    public void RemovePlanFromCalendar(int planIndex){
+        plans.remove(planIndex);
+    }
+    */
+
+    public void startPlan(int selectedPlan) {
+        this.selectedPlan = selectedPlan-1; //pad for input-to-index disparity
+    }
+
+    public void stopPlan(int selectedPlan) {
+        this.selectedPlan = -1;
+    }
+
+    public void showPlan(){
+        try {
+            System.out.println(planPresets.get(selectedPlan));
+
+            if (selectedPlan == -1) {
+                throw new NullPointerException();
+            }
+            System.out.println(planPresets.get(selectedPlan));
+        } catch(IndexOutOfBoundsException e){
+            System.out.println("Invalid index entered");
+        } catch (NullPointerException e){
+            System.out.println("No plans selected");
+        }
     }
 
 }
