705a87acf16bddc0ce565fea32205f018c403f03
Jiali <79980461+kemilii@users.noreply.github.com>
2025-03-20 23:10:28 +0800

    Replace wildcard static import with explicit static imports

--- src/test/java/pantrypal/shoppinglist/ShoppingListGeneratorTest.java
+++ src/test/java/pantrypal/shoppinglist/ShoppingListGeneratorTest.java
@@ -1,6 +1,10 @@
 package pantrypal.shoppinglist;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
