b45db656c1defcdade725fec31043573dc010722
Gyurim <gyurimkimyok@gmail.com>
2025-03-27 21:16:37 +0800

    Fixed StyleCheck

--- src/main/java/pantrypal/general/commands/inventory/AlertExpiredIngredient.java
+++ src/main/java/pantrypal/general/commands/inventory/AlertExpiredIngredient.java
@@ -1,28 +1,23 @@
 package pantrypal.general.commands.inventory;
 
 import pantrypal.general.control.Ui;
-import pantrypal.inventory.Ingredient;
 import pantrypal.inventory.IngredientInventory;
 import pantrypal.mealplan.PlanPresets;
 import pantrypal.recipe.RecipeManager;
 import pantrypal.shoppinglist.ShoppingList;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
 
 public class AlertExpiredIngredient extends InventoryCommand {
-
-public AlertExpiredIngredient() {
-    super("alertExpiredIngredient",
+    public AlertExpiredIngredient() {
+        super("alertExpiredIngredient",
             "Alerts the user about expired ingredients in the inventory.");
-}
-
-@Override
-public void execute(Ui ui, IngredientInventory inventory, ShoppingList list, PlanPresets presets,
-                    RecipeManager recipes, Scanner in) {
-    inventory.alertExpiredIngredient();
-
-}
+    }
+    @Override
+    public void execute(Ui ui, IngredientInventory inventory, ShoppingList list, PlanPresets presets,
+                        RecipeManager recipes, Scanner in) {
+        inventory.alertExpiredIngredient();
+    }
 }
 
