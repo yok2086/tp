bcc2a874f027af6cc5751192f3d2740eae6192cb
srfl0-admin <e1121549@u.nus.edu>
2025-04-03 20:56:14 +0800

    Refresh gradle wrapper

--- src/main/java/pantrypal/general/commands/mealplan/AddNewDay.java
+++ src/main/java/pantrypal/general/commands/mealplan/AddNewDay.java
@@ -24,6 +24,7 @@ public class AddNewDay extends MealPlanCommand {
     @Override
     public void execute(Ui ui, IngredientInventory inventory, ShoppingList list, PlanPresets presets,
                         RecipeManager recipes, Scanner in) {
-        
+
+
     }
 }
