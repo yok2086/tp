fea631c0cd9ac4abcdf22479701323862d8ad395
minhbgn <nhatminh003@e.ntu.edu.sg>
2025-04-03 18:02:28 +0800

    Add Frame for JUnit Test

---
+++ src/test/java/pantrypal/recipe/RecipeManagerTest.java
@@ -0,0 +1,62 @@
+package pantrypal.recipe;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pantrypal.inventory.Unit;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeManagerTest {
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void addRecipeIngredients() {
+    }
+
+    @Test
+    void addRecipeInstruction() {
+    }
+
+    @Test
+    void removeRecipeInstruction() {
+    }
+
+    @Test
+    void removeRecipeIngredient() {
+    }
+
+    @Test
+    void listRecipe() {
+    }
+
+    @Test
+    void showRecipe() {
+    }
+
+    @Test
+    void searchRecipe() {
+    }
+
+    @Test
+    void removeRecipe() {
+    }
+
+    @Test
+    void copyList() {
+    }
+
+    @Test
+    void getRecipeList() {
+    }
+}
